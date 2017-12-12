658acf4f08826ee95fc018db3656059434ca4143
Fnu Frangky <frank@FranksMBP.local>
2017-12-12 16:32:44 -0500

    Initial Commit

---
+++ Fnu_Frangky_Lab06/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fnu_Frangky_Lab06
+//
+//  Created by Fnu Frangky on 12/12/17.
+//  Copyright © 2017 Fnu Frangky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
